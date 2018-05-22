cddcc44cd1efb20ca6158464ad17d7449408d8df
clfhhc <clfhhc@gmail.com>
2018-05-21 17:39:13 -0700

    Have basic express server and client setup

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,30 @@
+var app = {};
+app.send = function(data){
+    $.ajax({
+        url: 'http://127.0.0.1:3000/',
+        type: 'POST',
+        dataType: 'application/json',
+        data: data,
+        success: function(response){
+            console.log('successfully post', response);
+        }
+    })
+}
 
+$(document).ready(function(){
+    $('#userInput').on('click', '.submit', function(event){
+        event.preventDefault();
+        let json = $("textarea#textarea").val();
+        if (json) {
+            try {
+                json = JSON.parse(json);
+                console.log(JSON.stringify(json));
+                app.send(json);
+            }
+            catch(err) {
+                console.log('interior error:', err);
+            }
+        }
+    })
+})
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,41 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.options('/', (req,res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.statusCode = 200;
+    res.end();
+})
+
+app.get('/', (req,res) => {
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, 'client/index.html'));
+    res.end();
+})
+
+app.post('/', (req,res)=> {
+    console.log('post "/":', req.body)
+    res.statusCode = 302;
+    res.end();
+})
+
+let server = http.createServer(app);
+
+server.listen(3000, '127.0.0.1');
